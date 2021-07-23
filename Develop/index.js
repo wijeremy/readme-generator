47af59c25246987e2ff0071ac85f9e203221d39b
wijeremy <jeremydavwilliams@gmail.com>
2021-07-23 17:52:11 -0500

    okay so i have the questions and the beginning of a md template

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,70 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs")
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [];
-
+// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: 'What year is it?',
+      name: 'year',
+    },
+    {
+      type: 'list',
+      message: 'Which license would you like to use?',
+      name: 'license',
+      options: ['BSD', 'MIT', 'GNU']
+    },
+    {
+        type:'input',
+        message: 'What was your motivation?',
+        name: 'motivation',
+    },
+    {
+        type:'input',
+        message: 'Why did you build this project?',
+        name: 'why',
+    },
+    {
+        type:'input',
+        message: 'What problem does it solve?',
+        name: 'problem',
+    },
+    {
+        type:'input',
+        message: 'What did you learn?',
+        name: 'learn',
+    },
+    {
+        type:'input',
+        message: 'What are the steps required to install your project?',
+        name: 'install',
+    },
+    {
+        type:'input',
+        message: 'Provide instructions and examples for use.',
+        name: 'use',
+    },
+    
+  ])
+  .then((response) =>
+    console.log(response.username)
+  );
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,14 @@
+function replaceString(text, string, newString) {
+  let arr = text.split(string);
+  let newText = arr.join(newString);
+  return newText;
+}
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -11,6 +19,8 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+//The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
 function generateMarkdown(data) {
   return `# ${data.title}
 
