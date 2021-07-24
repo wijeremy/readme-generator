9ffb28355860ab8add9ad94bf3fbd393dd436fcc
wijeremy <jeremydavwilliams@gmail.com>
2021-07-23 22:26:40 -0500

    probablyshouldn't have a random test file in there

--- Develop/test.js
+++
@@ -1,9 +0,0 @@
-function replaceString(text, string, newString) {
-    let arr = text.split(string);
-    let newText = arr.join(newString);
-    return newText;
-  };
-
-  let mystring = "hello my name is jeremy and i like to party";
-
-  console.log(replaceString(mystring, "jeremy", "the party monster"))
